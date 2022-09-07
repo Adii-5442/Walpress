2283472fea097115249733e3f2b43f27d7373c81
Aditya Singh <adi.sh5442@gmail.com>
2022-09-07 14:10:35 +0530

    First Commit

---
+++ app/src/main/java/com/example/walpress/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.walpress
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+
+        setContentView(R.layout.activity_main)
+    }
+}
