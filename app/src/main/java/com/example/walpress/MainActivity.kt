bf460b5c6fe2555551ca38513d67ce50cefc1c27
Aditya Singh <adi.sh5442@gmail.com>
2022-09-23 13:07:08 +0530

    minos improvements

--- app/src/main/java/com/example/walpress/MainActivity.kt
+++ app/src/main/java/com/example/walpress/MainActivity.kt
@@ -1,25 +1,34 @@
 package com.example.walpress
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        fun main(args : Array<String>) {
-            println("Kotlin is interesting.")
-        }
-        Log.e("Attention","My name is aditya")
-        for(i in 1..10){
-            println(i)
-        }
-
-
-
-
-
-
         setContentView(R.layout.activity_main)
+        var welcText = findViewById<TextView>(R.id.welcomeText)
+        welcText.setOnClickListener {
+            welcText.animate().apply {
+                duration = 1000
+                rotationX(360f)
+                rotationY(360f)
+            }.start()
+        }
+        var nextPage = findViewById<ImageView>(R.id.arrowRight)
+        nextPage.setOnClickListener {
+            Toast.makeText(this, "Going on Next Page", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+
     }
 }
